4f6ec1c838cedf3b89d54c979105a5f7a51e55ed
PrimalSword <106413088+PrimalSword@users.noreply.github.com>
2026-07-28 13:29:44 -0300

    build(rc1): set product candidate version 1.0.0-rc1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.orbistrade.dubaiai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.5.1"
+        versionCode = 10
+        versionName = "1.0.0-rc1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
