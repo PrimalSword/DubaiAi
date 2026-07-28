dd11cd3a7180e7f5d3ed57ab5db90f8c0251e97f
PrimalSword <106413088+PrimalSword@users.noreply.github.com>
2026-07-28 13:09:13 -0300

    chore: bump version to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,9 @@ android {
         applicationId = "com.orbistrade.dubaiai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.0"
+        versionCode = 7
+        versionName = "0.5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables.useSupportLibrary = true
     }
 
     buildFeatures { compose = true; buildConfig = true }
